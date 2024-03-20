f201d2b9be1ad4f660ad43611406d9711c3902d4
mohammad fuad <mohofoho@gmail.com>
2024-03-20 21:26:43 +0300

    .

--- JobDone/obj/Debug/net8.0/JobDone.AssemblyInfo.cs
+++ JobDone/obj/Debug/net8.0/JobDone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobDone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8aa38e1e1f9cfa882f58ded44794015626134e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2caa88d335201b9f6d58c02ef42cd7b8ab78aa6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobDone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobDone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
