16d24749348bfe2f6cda652f02b2cae805d9cf27
bazuhair01 <k3bol2015@gmail.com>
2024-03-25 00:12:42 +0300

    start with the SellerProfile

---
+++ JobDone/Models/SellerProfile/SellerProfileImplemntation.cs
@@ -0,0 +1,22 @@
+﻿using JobDone.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobDone.Models.SellerProfile
+{
+    public class SellerProfileImplemntation : ISellerProfile
+    {
+        private readonly DbSet<SellerModel> _seller;
+        private readonly DbSet<SellerOldWorkModel> _sellerOldWork;
+
+        public SellerProfileImplemntation(JobDoneContext context)
+        {
+            _seller = context.SellerModels;
+            _sellerOldWork = context.SellerOldWorkModels;
+        }
+        public List<SellerModel> GetSellerProfile(int sellerID)
+        {
+            return _seller.Where(x=>x.Id == sellerID).ToList();
+        }
+
+    }
+}
