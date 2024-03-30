ae9e9816264632dfbcc1f7f2e9a057f8b0487043
Faisal-Bojeir <158216002+Faisal-Bojeir@users.noreply.github.com>
2024-03-30 22:11:32 +0300

    Complete Modify Banner

--- JobDone/Controllers/Admin/AdminController.cs
+++ JobDone/Controllers/Admin/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using JobDone.Data;
 using JobDone.Models.Admin;
+using JobDone.Models.Banners;
 using JobDone.Models.Customer;
 using JobDone.Roles;
 using JobDone.ViewModels;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Security.Claims;
 
@@ -16,11 +18,12 @@ namespace JobDone.Controllers.Admin
     public class AdminController : Controller
     {
         private readonly IAdmin _admin;
+        private readonly IBanner _banner;
 
-        public AdminController(IAdmin admin) 
+        public AdminController(IAdmin admin, IBanner banner) 
         {
             _admin = admin;
-
+            _banner = banner;
         }
 
 
@@ -89,11 +92,37 @@ namespace JobDone.Controllers.Admin
         {
             return View();
         }
-        
-        public IActionResult EditBanners()
+
+        [HttpPost]
+        public async Task<IActionResult> EditBanners(string option)
         {
-            return View();
+            IEnumerable<BannerModel> banners;
+            if (option == "Customer")
+                banners = await _banner.GetAllCustomerBanners();
+            else if (option == "Seller")
+                banners = await _banner.GetAllSellerBanners();
+            else
+                banners = await _banner.GetAllAdminBanners();
+
+            ViewBag.option = option;
+            return View(banners);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(short picId, IFormFile picFile)
+        {
+            var banner = _banner.GetBannerById(picId);
+            _banner.ModeifyBanner(banner, picFile);
+            return RedirectToAction("EditBanners", ViewBag.option);
+        }
+
+        public async Task<IActionResult> EditBanners()
+        {
+            ViewBag.option = "Customer";
+            var banners = await _banner.GetAllCustomerBanners();
+            return View(banners);
+        }
+
         [AllowAnonymous]
         private async void SetAuthCookie(AdminModel admin)
         {
@@ -134,6 +163,12 @@ namespace JobDone.Controllers.Admin
                 CookieAuthenticationDefaults.CookiePrefix + options.Cookie.Name,
                 "JobDoneAdmin");
             
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login");
         }
 
         [HttpPost]
--- JobDone/Models/Banners/BannerImplementation.cs
+++ JobDone/Models/Banners/BannerImplementation.cs
@@ -6,20 +6,47 @@ namespace JobDone.Models.Banners
     public class BannerImplementation : IBanner
     {
         private readonly JobDoneContext _context;
+        private readonly DbSet<BannerModel> _banner;
 
         public BannerImplementation(JobDoneContext context)
         {
             _context = context;
+            _banner = _context.BannerModels;
+        }
+
+        public async Task<IEnumerable<BannerModel>> GetAllAdminBanners()
+        {
+            return await _banner.Where(forWho => forWho.ForWho == 3).ToListAsync();
         }
 
         public async Task<IEnumerable<BannerModel>> GetAllCustomerBanners()
         {
-            return await _context.BannerModels.Where(forWho => forWho.ForWho == 1).ToListAsync();
+            return await _banner.Where(forWho => forWho.ForWho == 1).ToListAsync();
         }
         
         public async Task<IEnumerable<BannerModel>> GetAllSellerBanners()
         {
-            return await _context.BannerModels.Where(forWho => forWho.ForWho == 2).ToListAsync();
+            return await _banner.Where(forWho => forWho.ForWho == 2).ToListAsync();
+        }
+
+        public BannerModel GetBannerById(int id)
+        {
+            return _banner.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void ModeifyBanner(BannerModel banner, IFormFile picFile)
+        {
+            if (banner != null && picFile != null)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    picFile.CopyTo(memoryStream);
+                    banner.Picture = memoryStream.ToArray();
+                }
+
+                _context.Update(banner);
+                _context.SaveChanges();
+            }
         }
     }
 }
