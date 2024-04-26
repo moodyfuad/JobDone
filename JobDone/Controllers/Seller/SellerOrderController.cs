35f90927478b67f27ffee8233c3994f2b3ae4860
Faisal-Bojeir <158216002+Faisal-Bojeir@users.noreply.github.com>
2024-04-26 20:43:22 +0300

    Complete chat functinality in seller and customer

--- JobDone/Controllers/Seller/SellerOrderController.cs
+++ JobDone/Controllers/Seller/SellerOrderController.cs
@@ -76,7 +76,7 @@ namespace JobDone.Controllers.Seller
             viewModel.Seller = new SellerModel();
             viewModel.Messages = _context.MessageModels.ToList();
             viewModel.Customer = _customer.GetCustomerById(customerId);
-            viewModel.Seller.Id = sellerId;
+            viewModel.Seller = _seller.GetSellerById(sellerId);
 
             if (string.IsNullOrEmpty(content))
             {
@@ -95,7 +95,7 @@ namespace JobDone.Controllers.Seller
 
             viewModel.Messages = _context.MessageModels.ToList();
 
-            return PartialView("Chat", new { customerId = customerId, sellerId = sellerId });
+            return PartialView("_SellerChatPartial", viewModel);
         }
 
         [HttpGet]
